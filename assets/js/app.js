a100a4da328ad258e717e4b10423efbc3d778e1f
Abby Johnson <brit13@gmail.com>
2018-03-22 17:23:12 -0700

    comparing user guesses to secret word

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,11 +3,16 @@ let guessedLetters = [];
 let guessesLeft = 10;
 let wins = 0;
 let secretWord;
+let guessedWord = "";
+let blanks = [];
+let guess;
 
 // Use key events to listen for the letters guessed
 document.onkeyup = function(e) {
     if(e.which>=65 && e.which<=90) {
-        console.log(e.key);
+        guess = e.key;
+        console.log(guess);
+        game.replaceLetters(guess);
     }
 };
 
@@ -21,22 +26,45 @@ document.onkeyup = function(e) {
 const game = {
     words: ["frabjous", "jabberwocky", "nonsense", "bandersnatch", "cheshire", "caterpillar"],
     randomWord() {
-        Math.floor(Math.random() * this.words.length);
+        let i = this.words.length;
+        secretWord = this.words[Math.floor(Math.random() * i)];
+        console.log(secretWord);
     },
+    letterBlanks() {
+        for (let i = 0; i < secretWord.length; i++) {
+            blanks.push("_");
+        };
+        console.log(guessedWord);
+        blanks.forEach((blank) => {
+            guessedWord += blank + " ";
+        });
+        console.log(blanks);
+        console.log("secret word: " + guessedWord);
+    }, 
+    replaceLetters(guess) {
+        const inWord = secretWord.indexOf(guess);
+        if(inWord>=0) {
+            for(let i=0; i<secretWord.length; i++) {
+                if(secretWord.charAt(i)===guess) {
+                    guessedWord[i]=guess;
+                    console.log(guessedWord)
+                }
+            };
+        }
+        console.log(inWord);
+    }
 
 }
 // ##### Word Guess Game Bonuses
 
 // Play a sound or song when the user guesses their word correctly, like in our demo.
-// Write some stylish CSS rules to make a design that fits your game's theme.
 // ** HARD MODE:** Organize your game code as an object
 // except for the key events to get the letter guessed.
-// This will be a challenge if you haven't coded with JavaScript before, 
-// but we encourage anyone already familiar with the language to try this out.
 // Save your whole game and its properties in an object.
 // Save any of your game's functions as methods, and call them underneath your object declaration using event listeners.
 // Don't forget to place your global variables and functions above your object.
 //     * Remember: global variables, then objects, then calls.
-// Definitely talk with a TA or your instructor if you get tripped up during this challenge.
 game.randomWord();
-console.log(game.words.length);
+game.letterBlanks();
+document.getElementById("secretWord").append(guessedWord);
+
