e02be0abf4ababa5b19974c7e6a1b2731b03a190
Abby Johnson <brit13@gmail.com>
2018-03-23 21:08:01 -0700

    updated function calls

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+// Global variables
 let userGuess;
 let guessedLetters = [];
 let guessesLeft = 10;
@@ -8,67 +9,74 @@ let blanks = [];
 let guess;
 
 // Use key events to listen for the letters guessed
-document.onkeyup = function(e) {
-    if(e.which>=65 && e.which<=90) {
+document.onkeyup = function (e) {
+    if (e.which >= 65 && e.which <= 90) {
         guess = e.key;
-        console.log(guess);
         game.replaceLetters(guess);
     }
 };
 
-// Choose one of the words from the list (repeat after each win/loss)
-
-
-// Replace letters with spaces
-
-// When correct letter is guessed, put it back into word
-
+// Save game and its properties in an object.
 const game = {
     words: ["frabjous", "jabberwocky", "nonsense", "bandersnatch", "cheshire", "caterpillar"],
     displaySecret() {
         document.getElementById("secretWord").innerHTML = guessedWord;
     },
+    // Choose one of the words from the list (repeat after each win/loss)
     randomWord() {
-        let i = this.words.length;
-        secretWord = this.words[Math.floor(Math.random() * i)];
-        console.log(secretWord);
+        if(this.words.length > 0) {
+            let i = Math.floor(Math.random() * this.words.length);
+            secretWord = this.words[i];
+            console.log(secretWord);
+            this.words.splice(i, 1);
+            console.log(this.words);
+        } else {
+            document.getElementById("secretWord").innerHTML = "You've played through all the words!";
+        }
     },
+    // Replace letters with spaces
     letterBlanks() {
         for (let i = 0; i < secretWord.length; i++) {
             blanks.push("_");
         };
-    }, 
+    },
+    // Convert blanks array into guessedWord string
     updateSecret() {
         guessedWord = '';
         blanks.forEach((blank) => {
             guessedWord += blank + " ";
         });
     },
+    // When correct letter is guessed, put it back into word
     replaceLetters(guess) {
+        guessedLetters.push(guess);
         const inWord = secretWord.indexOf(guess);
-        if(inWord>=0) {
-            for(let i=0; i<secretWord.length; i++) {
-                if(secretWord.charAt(i)===guess) {
-                    blanks[i]=guess;
+        if (inWord >= 0) {
+            for (let i = 0; i < secretWord.length; i++) {
+                if (secretWord.charAt(i) === guess) {
+                    blanks[i] = guess;
                 }
             };
             this.updateSecret()
             this.displaySecret();
+        } else {
+            guessesLeft--;
+            console.log("Guesses left: " + guessesLeft)
+            console.log("Guessed letters: " + guessedLetters);
         }
+    }, 
+    newWord() {
+        this.randomWord();
+        this.letterBlanks();
+        this.updateSecret();
+        this.displaySecret();
+        guessesLeft = 10;
+        guessedLetters = [];
     }
-    
 }
 // ##### Word Guess Game Bonuses
-
 // Play a sound or song when the user guesses their word correctly, like in our demo.
-// ** HARD MODE:** Organize your game code as an object
-// except for the key events to get the letter guessed.
-// Save your whole game and its properties in an object.
+
 // Save any of your game's functions as methods, and call them underneath your object declaration using event listeners.
-// Don't forget to place your global variables and functions above your object.
-//     * Remember: global variables, then objects, then calls.
-game.randomWord();
-game.letterBlanks();
-game.updateSecret();
-game.displaySecret();
+game.newWord(); 
 
