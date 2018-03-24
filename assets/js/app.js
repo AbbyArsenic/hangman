d1dbda1f0ed33429712414ddacb002c52aa366db
Abby Johnson <brit13@gmail.com>
2018-03-23 23:02:34 -0700

    update displays. Game over functionality is broken

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,50 @@
 // Global variables
-let userGuess;
+let guess;
+let secretWord;
 let guessedLetters = [];
+let blanks = [];
+let guessedWord = "";
 let guessesLeft = 10;
 let wins = 0;
-let secretWord;
-let guessedWord = "";
-let blanks = [];
-let guess;
 
 // Use key events to listen for the letters guessed
 document.onkeyup = function (e) {
     if (e.which >= 65 && e.which <= 90) {
         guess = e.key;
-        game.replaceLetters(guess);
+        game.checkGuess(guess);
     }
 };
 
 // Save game and its properties in an object.
 const game = {
-    words: ["frabjous", "jabberwocky", "nonsense", "bandersnatch", "cheshire", "caterpillar"],
-    displaySecret() {
-        document.getElementById("secretWord").innerHTML = guessedWord;
-    },
+    words: [
+        // "frabjous", 
+        // "jabberwocky", 
+        // "nonsense", 
+        // "bandersnatch", 
+        // "cheshire", 
+        // "caterpillar", 
+        // "wonderland", 
+        "dormouse", 
+        "curiouser"
+    ],
     // Choose one of the words from the list (repeat after each win/loss)
     randomWord() {
-        if(this.words.length > 0) {
+        console.log(this.words.length);
+        if (this.words.length >= 1) {
             let i = Math.floor(Math.random() * this.words.length);
             secretWord = this.words[i];
-            console.log(secretWord);
             this.words.splice(i, 1);
+            console.log(secretWord);
             console.log(this.words);
         } else {
-            document.getElementById("secretWord").innerHTML = "You've played through all the words!";
+            console.log("Game Over");
+            this.updateDisp("secretWord", guess);
         }
     },
     // Replace letters with spaces
     letterBlanks() {
+        blanks = [];
         for (let i = 0; i < secretWord.length; i++) {
             blanks.push("_");
         };
@@ -47,31 +56,49 @@ const game = {
             guessedWord += blank + " ";
         });
     },
-    // When correct letter is guessed, put it back into word
-    replaceLetters(guess) {
-        guessedLetters.push(guess);
-        const inWord = secretWord.indexOf(guess);
-        if (inWord >= 0) {
-            for (let i = 0; i < secretWord.length; i++) {
-                if (secretWord.charAt(i) === guess) {
-                    blanks[i] = guess;
-                }
-            };
-            this.updateSecret()
-            this.displaySecret();
-        } else {
+    // Check if guessed letter is in secret word
+    checkGuess(g) {;
+        const a = guessedLetters.indexOf(g);
+        const b = blanks.indexOf(g);
+        const inWord = secretWord.indexOf(g) >= 0;
+        console.log();
+        if (inWord) {
+            this.replaceLetters();
+        } else if (a === -1 && b === -1) {
+            guessedLetters.push(g);
             guessesLeft--;
-            console.log("Guesses left: " + guessesLeft)
-            console.log("Guessed letters: " + guessedLetters);
+            this.updateDisp("guessesLeft", guessesLeft);
+            this.updateDisp("lettersGuessed", guessedLetters);
+        };
+        this.checkWin();
+    },
+    // Put correct letters back into word
+    replaceLetters() {
+        for (let i = 0; i < secretWord.length; i++) {
+            if (secretWord.charAt(i) === guess) {
+                blanks[i] = guess;
+            }
+        };
+        this.updateSecret()
+        this.updateDisp("secretWord", guessedWord);;
+    }, 
+    checkWin() {
+        if (blanks.indexOf("_") === -1) {
+            wins++; 
+            this.updateDisp("wins", wins);
+            this.newWord();
         }
     }, 
     newWord() {
         this.randomWord();
         this.letterBlanks();
         this.updateSecret();
-        this.displaySecret();
+        this.updateDisp("secretWord", guessedWord);
         guessesLeft = 10;
         guessedLetters = [];
+    }, 
+    updateDisp(id, display) {
+        document.getElementById(id).innerHTML = display;
     }
 }
 // ##### Word Guess Game Bonuses
